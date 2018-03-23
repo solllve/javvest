4f2e331b62dec703a286ded7df2a81892ffc0492
solllve <we@solllve.com>
2018-03-22 20:55:54 -0700

    Splash page & Dashboard first

--- App.js
+++ App.js
@@ -1,13 +1,87 @@
-import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import React, {Component} from 'react';
+import { Image, Text, View, Button, Picker } from 'react-native';
+import { Asset, AppLoading } from 'expo';
+import { StackNavigator} from 'react-navigation';
 
-export default class Bananas extends Component {
+class SplashTimer extends React.Component {
+  state = {
+    ready: false,
+  }
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState({ ready: true })
+    }, 5000)
+  }
+
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
+    if (this.state.ready === false) {
+      return <SplashPage />
+    }
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <Dashboard />
+    );
+  }
+}
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <Text>Dashboard</Text>
+    );
+  }
+}
+
+
+//Splash Page
+class SplashPage extends React.Component {
+  state = {
+    isReady: false,
+  };
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <View style={{
+        flex: 1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
+      }}>
+        <Image style={{
+          flex: 1,
+          resizeMode: 'contain'
+        }} source={require('./assets/javvest-splash.png')} />
+      </View>
+    );
+  }
+
+  async _cacheResourcesAsync() {
+    const images = [
+      require('./assets/javvest-splash.png'),
+    ];
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages)
+
+  }
+}
+
+//Main App
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <SplashTimer />
+      </View>
     );
   }
 }
